35118ffec756c06e2e64e3572e72c660379bcdcc
jmcastilla <jmcastilla91@gmail.com>
2026-06-02 14:44:14 -0500

    prueba

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+
 const express = require('express');
 const mssql = require('mssql');
 const jwt = require('jsonwebtoken');
